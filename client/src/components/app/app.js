75b2d3d61a23d53ce989a6bfda77e75447e37fe8
Oleg Zasedatelev <ol.zasedatelew@yandex.ru>
2022-06-22 06:03:22 +0300

    client added

---
+++ client/src/components/app/app.js
@@ -0,0 +1,15 @@
+import './app';
+import SearchPanel from '../search-order/search-order';
+
+function App() {
+    return (
+        <div className="app">
+            
+            <div className="search-panel">
+                <SearchPanel/>
+            </div>
+        </div>
+    )
+}
+
+export default App;
